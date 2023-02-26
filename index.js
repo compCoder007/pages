f39fc3daff3306e77473c5632329adb908c50845
Umesh <competentcoder007@gmail.com>
2023-02-26 13:31:34 +0530

    Added a JavaScript file

---
+++ index.js
@@ -0,0 +1,2 @@
+n = prompt("Hi! What's your name?" );
+alert("Hi "+n+"\nThis page is created by 'compCoder007' hosted by Github pages.\nHope you like the page.\nThis site is still under development.\nYou can contact 'compCoder007' on github for any contribution for this page\nTHANK YOU");
